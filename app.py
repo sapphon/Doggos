58685e4890cdc7de03d14fca37a44fcbb4ba92f4
Connor Shaughnessy <sapphon@gmail.com>
2019-05-24 15:01:47 -0400

    Add static text and HTML template for index page

--- app.py
+++ app.py
@@ -3,12 +3,18 @@ import slack
 
 app = Flask(__name__)
 #client = Slack.WebClient(token=TOKENGOESHERE)
-corgi_url = 'https://preview.redd.it/be1fc3tlo5031.jpg?width=640&crop=smart&auto=webp&s=8c9d0eff07da7f6eac8142c3bdfc13b2ab658e3c'
 
+indexParameters = {
+    'title': 'Pets of Fordlabs',
+    'corgi_url': 'https://preview.redd.it/be1fc3tlo5031.jpg?width=640&crop=smart&auto=webp&s=8c9d0eff07da7f6eac8142c3bdfc13b2ab658e3c',
+    'let_them_down_gently': "We're sorry, but the service you are using encountered a problem.",
+    'heres_a_doggo': "To make up for it, we've got this cute pet to show you.",
+    'error_intro': "If you'd like to know more about the error you encountered, here's what we've got:"
+}
 @app.route('/doggo', methods=['GET'])
 def hello_world():
-    error_message = request.args.get('error')
-    return error_message
+    indexParameters['error_message'] = request.args.get('error')
+    return render_template('index.html', indexParameters=indexParameters)
 
 
 if __name__ == '__main__':
